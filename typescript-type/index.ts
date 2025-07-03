6411a763878ff5b98f03a842e6cbdfe46c33174b
tech-taku3 <tech.taku3@gmail.com>
2025-07-03 23:06:13 +0900

    section2:TypeScriptの型はこう書く_boolean, number, string

---
+++ typescript-type/index.ts
@@ -0,0 +1,9 @@
+let hasValue: boolean = true;
+// numberは浮動小数点も整数も負数も扱える
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+
+let single: string = 'hello';
+let double: string = "hello";
+let back: string = `hello`;
