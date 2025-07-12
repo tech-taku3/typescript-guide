a899b7cb8af0d5f243319c96009822944f341bce
tech-taku3 <tech.taku3@gmail.com>
2025-07-12 23:37:15 +0900

    Section4: TypeScriptではクラスをこう使う public修飾子とprivate修飾子を使用して、アクセスを制限する方法

--- typescript-type/notes-sec4.md
+++ typescript-type/notes-sec4.md
@@ -147,3 +147,33 @@ class Person {
 
 TypeScriptは`class Person`のときに、値を作ると同時に、
 そのクラスが生成するインスタンスの型も同名で作成する
+
+
+## public修飾子とprivate修飾子を使用して、アクセスを制限する方法
+
+```tsx
+class Person {
+    name: string; // デフォルトでpublic
+    private age: number; // privateをつけると、クラスの外側からアクセスできなくなる。
+    constructor(initName: string, initAge: number) {
+        this.name = initName;
+        this.age = initAge;
+    }
+
+    incrementAge() {
+        this.age += 1;
+    }
+
+    greeting(this: Person) {
+        console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
+    }
+}
+
+const quill = new Person("Quill", 38);
+quill.incrementAge();
+// quill.age = 42; // 直接自由に値を書き換えられるのよくない。 private　age: とすることでクラスの外側からアクセスできない。
+// console.log(quill.age); // 読み込むこともできない
+quill.greeting();
+```
+
+> コンパイルしたjsではprivateがない。※ES2022でprivateフィールド構文がjsに追加された(書き方は変わる)
--- typescript-type/src/class.ts
+++ typescript-type/src/class.ts
@@ -1,21 +1,22 @@
 class Person {
-    name: string;
-    constructor(initName: string) {
+    name: string; // デフォルトでpublic
+    private age: number; // privateをつけると、クラスの外側からアクセスできなくなる。
+    constructor(initName: string, initAge: number) {
         this.name = initName;
+        this.age = initAge;
     }
 
-    // greeting() {
-    // greeting(this: { name: string }) {
+    incrementAge() {
+        this.age += 1;
+    }
+
     greeting(this: Person) {
-        console.log(`Hello! My name is ${this.name}`);
+        console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
     }
 }
 
-const quill = new Person("Quill");
+const quill = new Person("Quill", 38);
+quill.incrementAge();
+// quill.age = 42; // 直接自由に値を書き換えられるのよくない。 private　age: とすることでクラスの外側からアクセスできない。
+// console.log(quill.age); // 読み込むこともできない
 quill.greeting();
-
-const anotherQuill = {
-    name: 'anotherQuill',
-    greeting: quill.greeting
-}
-anotherQuill.greeting() // この場合のthisはanotherQuillを指す。nameを設定していないため、undefinedを返す。
